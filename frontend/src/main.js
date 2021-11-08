98341fb35c594b48aa50017954951357e49cab15
Tom Suworof <rc1995king@gmail.com>
2021-11-08 12:03:06 +0300

    some tweaks

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,9 @@
 import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-//import 'bootstrap-vue/dist/bootstrap-vue.css'
-//import 'bootstrap/dist/css/bootstrap.css'
-//import BootstrapVue from 'bootstrap-vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
 createApp(App).use(router).use(store).mount('#app');
-// .use(BootstrapVue)
-//# sourceMappingURL=main.js.map
