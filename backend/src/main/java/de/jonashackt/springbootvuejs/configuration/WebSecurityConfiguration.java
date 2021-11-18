5c5902c11b94e8d2f8768ed94ec8d162b041b3a2
Tom Suworof <rc1995king@gmail.com>
2021-11-18 19:37:46 +0300

    some tweaks

--- backend/src/main/java/de/jonashackt/springbootvuejs/configuration/WebSecurityConfiguration.java
+++ backend/src/main/java/de/jonashackt/springbootvuejs/configuration/WebSecurityConfiguration.java
@@ -2,6 +2,7 @@ package de.jonashackt.springbootvuejs.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +19,12 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
